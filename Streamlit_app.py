a2e748e454497d787a22d75141bf662cd238a7ec
AnjalaideviV <168541918+AnjalaideviV@users.noreply.github.com>
2024-05-27 19:31:19 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sb
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
