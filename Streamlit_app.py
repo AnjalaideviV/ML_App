a38520347b8c232eb270330b5015ab99b82e08be
AnjalaideviV <168541918+AnjalaideviV@users.noreply.github.com>
2024-05-28 12:14:14 +0530

    m

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -56,7 +56,7 @@ st.pyplot(fig)
 
 # Data Preprocessing:
 st.header('Data Preprocessing:')
-
+columns=list(train.columns)
 
 st.write('Missing values in each column:')
 st.write(train.isnull().sum())
