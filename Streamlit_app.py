e695b29bb04fab8f0d88b87dfc9d69a9a9f977fd
AnjalaideviV <168541918+AnjalaideviV@users.noreply.github.com>
2024-07-16 17:56:57 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import seaborn as sb
 from sklearn.metrics import roc_curve,auc
 import warnings
